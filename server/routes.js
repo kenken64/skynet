3bb71ec24a31706c22b6accddff9a5c94b90c749
Nikhil Bhandari <nikhil.bhandari@intelligrape.com>
2016-06-29 19:44:57 +0800

    Removed usage of things

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,6 @@ module.exports = function (app) {
     // Insert routes below
     app.use('/api/v1/apps', require('./api/app'));
 
-    app.use('/api/things', require('./api/thing'));
     app.use('/api/users', require('./api/user'));
 
     app.use('/auth', require('./auth'));
